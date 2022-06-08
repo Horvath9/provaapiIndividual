62aa984afde4e83447c790c897ed68cc448228c5
horvath <pedro.montemurrog1@gmail.com>
2022-06-08 18:17:43 -0300

    update

--- api/src/repository/animeRepository.js
+++ api/src/repository/animeRepository.js
@@ -1,17 +1,17 @@
 import { con } from "./connection.js";
 
-export async function anime(nome){
+export async function anime(anime){
     const comando = `
     insert into tb_anime(nm_anime)
     values(?)`;
-    const[linhas]=await con.query(comando,[nome.nome])
-    nome.id = linhas.insertId
-    return nome;
+    const[linhas]=await con.query(comando,[anime.nome])
+    anime.id = linhas.insertId
+    return anime;
 }
 export async function consultaranime() {
 	const comando = `
 		SELECT id_anime			    id,
-			nm_anime nome
+			nm_anime anime
             from tb_anime`
 	const [linhas] = await con.query(comando);
 	return linhas;
